0e92118f8385e7def6d0a2fd8bf2095755e343a7
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-01-31 15:47:34 +0500

    sozlandi

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const mainURL = axios.create({
-    baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://mebelx-server.vercel.app/api",
 });
 
 export default mainURL;
-
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -6,38 +6,26 @@ import axios from "../../api";
 import "./Login.css";
 
 const Login = () => {
-  const [userId, setUserId] = useState("");
-  const [password, setPassword] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const { register, handleSubmit, reset } = useForm();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setIsPasswordVisible(!isPasswordVisible);
-  };
   const onSubmit = async (data) => {
     setLoading(true);
     try {
-      const res = await axios.post("/api/signin", data);
-      if (res.data.token) {
-        const { user, token } = res.data;
-        localStorage.setItem("token", token);
-        localStorage.setItem("Id", user._id);
-        localStorage.setItem("role", user.role);
-
-        navigate(`/${user.role}`);
-        message.success("Tizimga kirish muvaffaqiyatli yakunlandi!");
-      } else {
-        message.error("Kirishda xatolik yuz berdi");
-      }
+      const res = await axios.post("/admin/login", data);
+      console.log(res);
+      message.success(res.data.message);
+      localStorage.setItem("admin", JSON.stringify(res.data?.innerData?.admin));
+      localStorage.setItem("token", res.data?.innerData?.token);
+      localStorage.setItem("role", res.data?.innerData?.admin?.role);
+      navigate(`/${res.data?.innerData?.admin?.role}`);
     } catch (error) {
-      message.error("Kirishda xatolik yuz berdi");
-      console.error(error);
+      message.error(error.response.data.message);
     } finally {
       setLoading(false);
     }
-
     reset();
   };
 
@@ -47,20 +35,14 @@ const Login = () => {
         <h1>Kirish</h1>
         <div className="input-div">
           <p>Login</p>
-          <input
-            type="text"
-            className="input"
-            {...register("phone")}
-            value={userId}
-            onChange={(e) => setUserId(e.target.value)}
-          />
+          <input type="text" className="input" {...register("login")} />
         </div>
 
         <div className="input-div">
           <div className="i">
             <i
               className={`fas ${isPasswordVisible ? "fa-eye-slash" : "fa-eye"}`}
-              onClick={togglePasswordVisibility}
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
             ></i>
           </div>
           <p>Parol</p>
@@ -68,8 +50,6 @@ const Login = () => {
             type={isPasswordVisible ? "text" : "password"}
             className="input"
             {...register("password")}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
