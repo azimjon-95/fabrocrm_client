404abcb2417961febeb3c37e08e725224309944b
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-03-04 17:10:00 +0500

    change api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const mainURL = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
   // baseURL: "https://mebelx.abdujabborov.uz/api",
+  baseURL: "https://mebelx-server-three.vercel.app/api",
 });
 
 export default mainURL;
