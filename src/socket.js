77e26b59025129b1e1c72930d65bdb8234e63c14
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-04-22 09:56:14 +0500

    chage api

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = `http://localhost:5000`;
-// const SOCKET_URL = `https://mebelx.abdujabborov.uz/`;
+// const SOCKET_URL = `http://localhost:5000`;
+const SOCKET_URL = `https://mebelx.abdujabborov.uz/`;
 const socket = io(SOCKET_URL, { transports: ["websocket"] });
 
 export default socket;
