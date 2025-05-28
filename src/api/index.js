84b308ddb572fd2460f325e3a2a8139adf5007c0
azimjon-95 <>
2025-05-28 15:54:09 +0500

    toy 4454

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const mainURL = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://mebelx.abdujabborov.uz/api",
-  // baseURL: "https://mebelx-server-three.vercel.app/api",
+  // baseURL: "https://mebelx.abdujabborov.uz/api",
+  baseURL: "https://mebelx-server-three.vercel.app/api",
 });
 
 export default mainURL;
